ef14beac88dbd0d859b9e6f997c683f66d39a905
rob-adt <r.weeks0308@gmail.com>
2025-08-20 08:53:33 +0100

    .

--- sitebag/bags/models.py
+++ sitebag/bags/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 class Bags(models.Model):
     inbag=models.CharField(max_length=200)
+    def __str__(self):
+        return self.inbag
 
 class Borrowingtime(models.Model):
     start=models.DateTimeField("date borrowed")
@@ -12,4 +14,7 @@ class Borrowingtime(models.Model):
 
 
 class Employees(models.Model):
+    employee_name=models.CharField(max_length=100)
     borrowed = models.ForeignKey(Borrowingtime, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.employee_name
