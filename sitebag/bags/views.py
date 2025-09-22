f016f85a20d8e5249427a2dc668cbc8c3be2977c
rob-adt <r.weeks0308@gmail.com>
2025-09-22 15:27:52 +0100

    /

--- sitebag/bags/views.py
+++ sitebag/bags/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import Bag,Employee,Borrowingtime
 from django.db.models import Max
@@ -19,8 +19,16 @@ def index(request):
             bag.being_used = bag.last_borrowingtime.end is None
 
     context["authenticated"] = request.user.is_authenticated
-    username=request.user.username
-   
+    context["username"]=request.user.username
+
+    if bag.being_used == True:
+        buttoo=False
+    else:
+        buttoo=True
+
+    context["buttoo"]=buttoo
+
+
     return render(request, "sitebag/detail.html", context=context)
     
 from django.contrib.auth import authenticate, login
@@ -30,9 +38,17 @@ def my_view(request):
     username = request.POST["username"]
     password = request.POST["password"]
     user = authenticate(request, username=username, password=password)
+    username="fish"
     if user is not None:
         login(request, user)
+        
         # Redirect to a success page.
     else:
         # Return an 'invalid login' error message.
         ...
+    return render(request,"sitebag/detail.html",username=username)
+
+
+def borrowbagview(request, pk):
+    print(get_object_or_404(Bag, pk=pk))
+    return redirect("index")
